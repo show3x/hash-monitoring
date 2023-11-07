79f68a2fffed7fddb9222a507e3134b0fcdc93c9
show3x <99814626+show3x@users.noreply.github.com>
2023-11-07 22:55:41 +0300

    Update monitoring.py

--- monitoring.py
+++ monitoring.py
@@ -19,7 +19,7 @@ def calculate_hash(file_path, hash_method):
             file_hash.update(content)
             return file_hash.hexdigest()
     except Exception as e:
-        print(f"Ошибка при вычислении хеша файла {file_path}: {str(e)}")
+        print(f"Error when calculating the hash of a file {file_path}: {str(e)}")
         return None
 
 # Функция для мониторинга файлов
@@ -30,8 +30,8 @@ def start_monitoring():
                 if os.path.isfile(monitored_file):  # Проверяем, существует ли файл
                     current_hash = calculate_hash(monitored_file, hash_method) # Вычисляется хеш текущего файла с использованием заданного метода хеширования (hash_method), и результат сохраняется в current_hash.
                     if monitored_file in file_hashes and file_hashes[monitored_file] != current_hash:
-                        send_notification('Хэш файла был изменен')
-                        print('Хэш файла был изменен')
+                        send_notification('file hash has been changed')
+                        print('file hash has been changed')
                         plyer.notification.notify(
                             message='file hash has been changed',
                             app_name='monitoring',
@@ -40,4 +40,4 @@ def start_monitoring():
                     file_hashes[monitored_file] = current_hash # Обновляется значение хеша в словаре file_hashes для данного файла.
                 time.sleep(10)
     except KeyboardInterrupt:
-        print("Программа завершена.")
+        print("Program Completed.")
