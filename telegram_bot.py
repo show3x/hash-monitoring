d7efb95a3ae91b42b67f2457e95feb6e885d0b4a
show3x <99814626+show3x@users.noreply.github.com>
2023-11-07 22:31:07 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -11,12 +11,12 @@ photo_url = 'https://uznayvse.ru/images/stories2016/uzn_1473155502.jpg'
 @bot.message_handler(commands=['start'])
 def hello(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-    keyboard.add(types.KeyboardButton('Получить Стэтхема'))
-    bot.send_message(message.chat.id, f'Здравствуй {message.from_user.first_name}!', reply_markup=keyboard)
+    keyboard.add(types.KeyboardButton('Get Statham'))
+    bot.send_message(message.chat.id, f'Hello {message.from_user.first_name}!', reply_markup=keyboard)
 
 @bot.message_handler(content_types=['text'])
 def main_menu(message):
-    if message.text == 'Получить Стэтхема':
+    if message.text == 'Get Statham':
         bot.send_photo(message.chat.id, photo=photo_url, caption='If you close your eyes, it gets dark\n(c)Jason Statham')
 
 # Функция для отправки уведомления
